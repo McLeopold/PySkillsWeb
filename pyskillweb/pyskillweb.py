8acc37ffd2d8ccdbba127c903a942cc397431f5f
McLeopold <mcleopold@gmail.com>
2012-03-20 17:40:49 -0700

    added debugging and routing rules

--- pyskillweb/pyskillweb.py
+++ pyskillweb/pyskillweb.py
@@ -1,35 +1,137 @@
 from svana import Svana
 
-app = Svana('/', db='/home/scott/test/config.sqlite')
+app = Svana('/',
+            db='/home/scott/test/config.sqlite',
+            host='172.16.0.9:8000',
+            debug=True)
 
-@app.route('/<name>')
-class HomePage():
+def make_id_gen(name):
+    if name not in make_id_gen.data:
+        make_id_gen.data[name] = 0
+    def get_id():
+        make_id_gen.data[name] += 1
+        return str(make_id_gen.data[name])
+    return get_id
+make_id_gen.data = {}
 
-    def get(self, name):
-        return "Hello World {0}".format(name)
-        
-    def post(self, name):
-        return "Goodbye World {0}".format(name)
+next_contest_id = make_id_gen('contest')
+
+class Contest(object):
+    def __init__(self, contest_id, name):
+        self.contest_id = contest_id
+        self.name = name
+        self.players = Players(self)
+        self.games = {}
+    def new_player(self, player_id):
+        if player_id not in self.players:
+            self.players[player_id] = Player(player_id, self)
+    def data(self):
+        return {'contest_id': self.contest_id,
+                'name': self.name,
+                'link': app.url_for(ContestRoute.get,
+                                    contest_id=self.contest_id),
+                'players': app.url_for(PlayersRoute.get,
+                                       contest_id=self.contest_id),
+                'games': app.url_for(GamesRoute.get,
+                                     contest_id=self.contest_id)}
+
+class Players(dict):
+    def __init__(self, contest):
+        self.contest = contest
+    def data(self):
+        return {'count': len(self),
+                'players': {key: app.url_for(PlayerRoute.get,
+                                             contest_id=self.contest.contest_id,
+                                             player_id=key)
+                            for key in self}}
+
+class Player(object):
+    def __init__(self, player_id, contest):
+        self.player_id = player_id
+        self.contest = contest
+    def data(self):
+        return {'player_id': self.player_id,
+                'link': app.url_for(PlayerRoute.get,
+                                    contest_id=self.contest.contest_id,
+                                    player_id=self.player_id)}
+
+contests = {}
+sample_contest = Contest('0', u'\u2126sample')
+sample_contest.new_player(u'b\u00f8b')
+contests[sample_contest.contest_id] = sample_contest
+
+@app.route_json('/contests')
+class ContestsRoute(object):
+
+    def get(self, req):
+        'Get list of contests'
+        return {key: app.url_for(ContestRoute.get, contest_id=key)
+                           for key in contests}
+
+    def post(self, req):
+        'Create new contest with name'
+        contest_id = next_contest_id()
+        contests[contest_id] = Contest(contest_id, req.json['name'])
+        return {contest_id: app.url_for(ContestRoute.get, contest_id=contest_id)}
+
+@app.route_json('/contests/<contest_id>')
+class ContestRoute(object):
+
+    def get(self, req, contest_id):
+        'Get contest info'
+        contest = contests[contest_id]
+        return contest.data()
 
 
-class API():
+@app.route_json('/contests/<contest_id>/players')
+class PlayersRoute(object):
 
-    @app.route('/api', ['GET'])
-    def get(self):
-        return "Hello World"
-        
-    @app.route('/api', ['POST'])
-    def post(self):
-        return "Goodbye World"
-        
-        
-@app.route('/test/<name>')
-def test(name):
-    return 'test {0}'.format(name)
+    def get(self, req, contest_id):
+        contest = contests[contest_id]
+        return {key: app.url_for(PlayerRoute.get,
+                                 contest_id=contest_id,
+                                 player_id=key)
+                for key in contest.players}
 
-if __name__ == '__main__':        
+    def put(self, req, contest_id):
+        'Create new player in contest'
+        contest = contests[contest_id]
+        player_id = req.json['player_id']
+        if player_id not in contest.players:
+            contest.new_player(player_id)
+        return contest.players[player_id].data()
+
+@app.route_json('/contests/<contest_id>/players/<player_id>')
+class PlayerRoute(object):
+
+    def get(self, req, contest_id, player_id):
+        raise TypeError
+        contest = contests[contest_id]
+        player = contest.players[player_id]
+        return player.data()
+
+@app.route_json('/contests/<contest_id>/games')
+class GamesRoute(object):
+
+    def get(self, req, contest_id):
+        contest = contests[contest_id]
+        return contest.games
+
+@app.route_json('/')
+def list_routes(req):
+    routes = []
+    for rule in app.url_map.iter_rules():
+        if 'GET' in rule.methods:
+            try:
+                routes.append(app.url_for(rule.endpoint))
+            except:
+                pass
+    return routes
+
+if __name__ == '__main__':
+    for rule in app.url_map.iter_rules():
+        print(rule)
     try:
         app.run()
     except KeyboardInterrupt:
         print('shutting down...')
-
--- pyskillweb/svana.py
+++ pyskillweb/svana.py
@@ -1,27 +1,41 @@
-from werkzeug.routing import Map, Rule
-from werkzeug.exceptions import HTTPException, abort
-from werkzeug.urls import url_quote
+from werkzeug.routing import Map, Rule, UnicodeConverter
+from werkzeug.exceptions import HTTPException, InternalServerError
+from werkzeug.urls import url_quote, url_unquote_plus
 from werkzeug.debug import DebuggedApplication
+from werkzeug.wrappers import BaseRequest, BaseResponse
 from mongrel2.handler import Connection
 from mongrel2.request import Request
 from mongrel2.config import model
 from uuid import uuid4
 import sys
+import types
+from cStringIO import StringIO
+import json
 
 WSGI_FORMAT = 'HTTP/1.1 {status}\r\n{headers}\r\n\r\n{body}'
+
+class UnquoteConverter(UnicodeConverter):
+
+    def to_python(self, value):
+        return url_unquote_plus(value)
+
+class JSONResponse(BaseResponse):
+    default_mimetype = 'application/json; charset=utf-8'
 
 class SvanaConnection(Connection):
     def recv(self):
         req_str = self.reqs.recv()
-        print(req_str)
+        #print(req_str)
         return Request.parse(req_str)
 
     def reply_wsgi(self, req, app, environ=None):
         # setup wsgi environment & response callable
         if environ is None:
-            environ = {'REQUEST_METHOD': req.headers['METHOD'],
+            environ = {'REQUEST_URI': req.path,
+                       'REQUEST_METHOD': req.headers.get('METHOD', 'HEAD'),
                        'QUERY_STRING': req.headers.get('QUERY', ''),
-                       'wsgi.errors': None}
+                       'wsgi.errors': sys.stdout,
+                       'wsgi.input': StringIO(req.body)}
         payload = {'status': None,
                    'headers': None}
         body = []
@@ -30,7 +44,6 @@ class SvanaConnection(Connection):
             payload['headers'] = headers
             return body.append
         # call app
-        app = DebuggedApplication(app, show_hidden_frames=True)
         app_iter = app(environ, start_response)
         try:
             for item in app_iter:
@@ -40,13 +53,39 @@ class SvanaConnection(Connection):
                 app_iter.close()
         # format response
         payload['body'] = ''.join(body)
-        header_keys = set(key.lower() for key, value in payload['headers'])
-        if 'content-length' not in header_keys:
-            payload['headers'].append(('Content-Length', len(payload['body'])))    
         payload['headers'] = '\r\n'.join('{0}: {1}'.format(k, v)
                                          for k, v in payload['headers'])
         response = WSGI_FORMAT.format(**payload)
         self.reply(req, response)
+
+class SvanaDebugged(DebuggedApplication):
+    def __call__(self, environ, start_response):
+        """Dispatch the requests."""
+        # important: don't ever access a function here that reads the incoming
+        # form data!  Otherwise the application won't have access to that data
+        # any more!
+        request = BaseRequest(environ)
+        response = self.debug_application
+        if request.args.get('__debugger__') == 'yes':
+            cmd = request.args.get('cmd')
+            arg = request.args.get('f')
+            secret = request.args.get('s')
+            traceback = self.tracebacks.get(request.args.get('tb', type=int))
+            frame = self.frames.get(request.args.get('frm', type=int))
+            if cmd == 'resource' and arg:
+                response = self.get_resource(request, arg)
+            elif (cmd == 'paste' and traceback is not None 
+                    and secret == self.secret):
+                response = self.paste_traceback(request, traceback)
+            elif cmd == 'source' and frame and self.secret == secret:
+                response = self.get_source(request, frame)
+            elif (self.evalex and cmd is not None and frame is not None and
+                    self.secret == secret):
+                response = self.execute_command(request, cmd, frame)
+        elif (self.evalex and self.console_path is not None and
+                request.path == self.console_path):
+            response = self.display_console(request)
+        return response(environ, start_response)
 
 class Svana():
 
@@ -54,16 +93,18 @@ class Svana():
                  sender_id=None,
                  host='localhost',
                  db='config.sqlite',
-                 default_methods=None):
-        self.mongrel_db = None
-        self.url_map = Map()
+                 default_methods=None,
+                 debug=False):
+        self.url_map = Map(converters={'default': UnquoteConverter})
         self.url_adapter = self.url_map.bind(host)
         self.default_methods = default_methods or ['GET']
+        self.endpoint_lookup = {}
         self.sender_id = sender_id or uuid4().hex
         if isinstance(route, (str, unicode)):
             route = self.get_pub_sub(route, db)
         self.pub, self.sub = route
-        
+        self.debug = debug
+
     def get_pub_sub(self, route, db=None):
         if db is None:
             db = 'config.sqlite'
@@ -79,27 +120,45 @@ class Svana():
         else:
             raise Exception('route is not unique: {0}'.format(', '.join()))
 
-    def route(self, url, methods=None):
+    def route(self, url, methods=None, wrapper=None):
         def hitch(obj):
-            route_methods = methods
-            if methods is None:
-                if hasattr(obj, '__call__'):
-                    route_methods = self.default_methods
-                else:
-                    route_methods = [prop for prop in obj.__dict__
-                                     if hasattr(getattr(obj, prop), '__call__')]
-            self.url_map.add(Rule(url,
-                                  endpoint=obj,
-                                  methods=route_methods))
+            if isinstance(obj, types.FunctionType):
+                route_methods = methods or self.default_methods
+                endpoint = wrapper(obj) if wrapper else obj
+                self.endpoint_lookup[obj] = endpoint
+                self.url_map.add(Rule(url, endpoint=endpoint, methods=route_methods))
+            else:
+                for prop in list(obj.__dict__.keys()):
+                    method = getattr(obj, prop)
+                    if hasattr(method, '__call__'):
+                        def instantiator(req, prop=prop, *args, **kwds):
+                            instance = obj()
+                            return getattr(instance, prop)(req, *args, **kwds)
+                        endpoint = wrapper(instantiator) if wrapper else instantiator
+                        self.url_map.add(Rule(url, endpoint=endpoint, methods=[prop]))
+                        self.endpoint_lookup[method] = endpoint
             return obj
         return hitch
+
+    def route_json(self, url, methods=None):
+        def json_wrapper(fn):
+            def go(req, *args, **kwds):
+                req.json = json.loads(req.body) if req.body else None
+                res = fn(req, *args, **kwds)
+                return JSONResponse(json.dumps(res, ensure_ascii=False))
+            return go
+        return self.route(url, methods, json_wrapper)
 
     def url_for(self, endpoint, method=None, external=False,
                 append_unknown=True, anchor=None, **values):
         if endpoint not in self.url_map._rules_by_endpoint:
+            # check for method
+            if endpoint in self.endpoint_lookup:
+                endpoint = self.endpoint_lookup[endpoint]
             # check for unbound method
-            if hasattr(endpoint, 'im_class'):
+            elif hasattr(endpoint, 'im_class'):
                 endpoint = endpoint.im_class.__dict__[endpoint.__name__]
+        print('building for {0} values {1}'.format(endpoint, values))
         rv = self.url_adapter.build(endpoint, values,
                                     method=method,
                                     force_external=external,
@@ -108,38 +167,46 @@ class Svana():
             rv += '#' + url_quote(anchor)
         return rv
         
-    def dispatch(self, path_info=None, method=None):
-        def app(environ, start_response):
-            print('dispatching {0} {1}...'.format(path_info, method))
-            endpoint, arguments = self.url_adapter.match(path_info, method)
-            print('found endpoint {0}\n'.format(endpoint))
-            try:
-                start_response('200 OK', [])
-                if hasattr(endpoint, '__call__'):
-                    return [endpoint(**arguments)]
-                else:
-                    endpoint_class = endpoint()
-                    return [getattr(endpoint_class, method.lower())(**arguments)]
-            except TypeError:
-                print('failed to call endpoint')
-                #abort(500)
-                raise
-        return DebuggedApplication(app)
+    def dispatch(self, req):
+        path = req.path
+        method = req.headers.get('METHOD', 'HEAD')
+        for key, value in req.headers.items():
+            print(key, value)
+        print('dispatching {0} {1} ... '.format(path, method))
+        if req.body:
+            print(req.body)
+        endpoint, arguments = self.url_adapter.match(path, method)
+        print('found endpoint {0}'.format(endpoint))
+        result = endpoint(req, **arguments)
+        if not isinstance(result, BaseResponse):
+            result = BaseResponse(result)
+        return result
 
     def run(self):
+        # setup debugging if needed
         conn = SvanaConnection(self.sender_id, self.pub, self.sub)
-        while True:
-            print("WAITING FOR REQUEST...")
-            req = conn.recv()
-            if req.should_close():
-                print("DISCONNECT")
-                continue
-            try:
-                app = self.dispatch(req.path, req.headers.get('method', 'get')) or ''
-                conn.reply_wsgi(req, app)
-#                print(res)
-#                conn.reply_http(req, res)
-            except HTTPException as e:
-                print('{req.sender} {req.path} {req.conn_id} - {error}\n'.format(req=req, error=e))
-                conn.reply_wsgi(req, e)
+        if self.debug:
+            def app(environ, start_response):
+                result = self.dispatch(req)
+                return result(environ, start_response)
+            app = SvanaDebugged(app, evalex=True)
+            while True:
+                req = conn.recv()
+                try:
+                    conn.reply_wsgi(req, app)
+                except HTTPException as e:
+                    print('failing debug mode')
+                    print('{req.sender} {req.path} {req.conn_id} - {error}\n'.format(req=req, error=e))
+                    conn.reply_wsgi(req, e)
+        else:
+            while True:
+                req = conn.recv()
+                try:
+                    res = self.dispatch(req)
+                    conn.reply_wsgi(req, res)
+                except HTTPException as e:
+                    conn.reply_wsgi(req, e)
+                except Exception as e:
+                    print(e)
+                    conn.reply_wsgi(req, InternalServerError())
 
